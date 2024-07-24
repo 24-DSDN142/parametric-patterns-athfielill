11d923a2fb8486233679aadf1b324bf68e1d0c94
athfielill <lilly.athfield@gmail.com>
2024-07-25 11:44:24 +1200

    Update my_wallpaper.js
    just some squares

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,5 +1,5 @@
 //your parameter variables go here!
-let rect_width  = 20;
+let rect_width = 20;
 let rect_height = 20;
 
 
@@ -9,9 +9,9 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
+  pWallpaper.grid_settings.cell_width = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset = 50;
 }
 
 function wallpaper_background() {
@@ -19,5 +19,9 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+
+  rect(40, 40, rect_width, rect_height);
+
+  rect(60, 40, rect_width, rect_height);
+
 }
