1e8ee5993858704f08867e66cf4a2eee249e4fd7
athfielill <lilly.athfield@gmail.com>
2024-07-25 16:47:38 +1200

    Update my_wallpaper.js
    watermelon

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -10,7 +10,7 @@ function setup_wallpaper(pWallpaper) {
 
   //Grid settings
   pWallpaper.grid_settings.cell_width = 200;
-  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.cell_height = 100;
   pWallpaper.grid_settings.row_offset = 0;
 }
 
@@ -23,52 +23,56 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   angleMode(RADIANS)
 
 
-  DrawLemonSlice(200 / 2, 200 / 2);
+  // DrawLemonSlice(200 / 2, 200 / 2);
 
 
 
+  // let arc1x = 100
+  // let arc1y = 100
 
-  // let circleX = 100;
-  // let circleY = 100;
-  // stroke(245, 228, 103);
-  // fill(245, 228, 103);
-  // ellipse(circleX, CircleY, 89, 89); // outer circle
+  // strokeWeight(0);
+  // fill(255, 173, 20);//dark yellow
+  // arc(arc1x, arc1y, 100, 200, 20, PI);//outer semi circle
 
-  // stroke(255);//white rind of lemon
-  // strokeWeight(4);
-  // fill(245, 240, 103);//yellow
-  // ellipse(circleX, CircleY, 75, 75); // small circle
-
-
+  // stroke(255, 255, 255);
   // strokeWeight(2);
-  // stroke(255);//white
-  // line(circleX - 38, CircleY, circleX + 37, CircleY);//horizontal white line
-  // line(circleX, CircleY - 38, circleX, CircleY + 27);//vertical line
-  // line(circleX - 25, CircleY - 25, circleX + 27, CircleY + 27);//diagonal line
-  // line(circleX + 27, CircleY - 25, circleX - 25, CircleY + 27);//diagonal line
+  // fill(255, 227, 89);//lighter yellow
+  // ellipse(100, 110, 90, 80);
 
+  // line(arc1x, arc1y, arc1x, arc1y + 45);
+
+  // line(arc1x, arc1y, arc1x - 31, arc1y + 32);
+  // line(arc1x, arc1y, arc1x + 31, arc1y + 31);
+  // line(arc1x - 45, arc1y, arc1x + 45, arc1y);
+
+
+  fill(235, 70, 89)// red
+  triangle(20, 75, 60, 20, 90, 75); // red triangle
+  triangle(180, 20, 110, 20, 145, 75); // red triangle 2
+
+  line()
 
 
 
 }
 
-function DrawLemonSlice(arc1x, arc1y) {
+// function DrawLemonSlice(arc1x, arc1y) {
 
 
 
-  strokeWeight(0);
-  fill(255, 173, 20);//dark yellow
-  arc(arc1x, arc1y, 100, 100, 0, PI);//outer semi circle
+//   strokeWeight(0);
+//   fill(255, 173, 20);//dark yellow
+//   arc(arc1x, arc1y, 100, 100, 0, PI);//outer semi circle
 
-  stroke(255, 255, 255);
-  strokeWeight(2);
-  fill(255, 227, 89);//lighter yellow
-  arc(arc1x, arc1y, 90, 90, 0, PI);//inner semi circle
+//   stroke(255, 255, 255);
+//   strokeWeight(2);
+//   fill(255, 227, 89);//lighter yellow
+//   arc(arc1x, arc1y, 90, 90, 0, PI);//inner semi circle
 
-  line(arc1x, arc1y, arc1x, arc1y + 45);
+//   line(arc1x, arc1y, arc1x, arc1y + 45);
 
-  line(arc1x, arc1y, arc1x - 31, arc1y + 32);
-  line(arc1x, arc1y, arc1x + 31, arc1y + 31);
-  line(arc1x - 45, arc1y, arc1x + 45, arc1y);
+//   line(arc1x, arc1y, arc1x - 31, arc1y + 32);
+//   line(arc1x, arc1y, arc1x + 31, arc1y + 31);
+//   line(arc1x - 45, arc1y, arc1x + 45, arc1y);
 
-}
+// }
