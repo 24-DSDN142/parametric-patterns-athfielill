2c0808443e96a2784e93a75acbdfbba2a9d6b686
athfielill <lilly.athfield@gmail.com>
2024-07-26 09:59:38 +1200

    Update my_wallpaper.js
    watermelon

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -46,11 +46,68 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   // line(arc1x - 45, arc1y, arc1x + 45, arc1y);
 
 
-  fill(235, 70, 89)// red
-  triangle(20, 75, 60, 20, 90, 75); // red triangle
-  triangle(180, 20, 110, 20, 145, 75); // red triangle 2
 
-  line()
+
+  //watermelon 1
+  stroke(235, 70, 89)
+  fill(235, 70, 89)// red
+  triangle(20, 75, 60, 20, 95, 75); // red triangle
+
+
+  stroke(34, 143, 63)
+  fill(34, 143, 63); //green
+  ellipse(57, 80, 81, 15) // green rind ellipse
+
+
+  stroke(255)
+  fill(255); //white
+  ellipse(57, 77, 80, 12) // white rind ellipse
+
+
+  stroke(235, 70, 89)
+  fill(235, 70, 89); //red
+  ellipse(58, 74, 74, 10) // red rind ellipse
+
+  stroke(5)
+  fill(5); //black
+  ellipse(60, 35, 2, 4) // black seed
+  ellipse(47, 45, 2, 4) // black seed
+  ellipse(70, 50, 2, 4) // black seed
+  ellipse(52, 60, 2, 4) // black seed
+  ellipse(35, 67, 2, 4) // black seed
+  ellipse(79, 70, 2, 4) // black seed
+  ellipse(60, 75, 2, 4) // black seed
+
+  // watermelon 2
+
+  stroke(235, 70, 89)
+  fill(235, 70, 89)// red
+  triangle(190, 20, 110, 20, 150, 75); // red triangle 2
+
+
+  stroke(34, 143, 63)
+  fill(34, 143, 63); //green
+  ellipse(150, 20, 81, 15) // green rind ellipse
+
+
+  stroke(255)
+  fill(255); //white
+  ellipse(150, 23, 78, 12) // white rind ellipse
+
+
+  stroke(235, 70, 89)
+  fill(235, 70, 89); //red
+  ellipse(150, 25, 73, 10) // red rind ellipse
+
+  stroke(5)
+  fill(5); //black
+  ellipse(160, 31, 2, 4) // black seed
+  ellipse(127, 28, 2, 4) // black seed
+  ellipse(160, 50, 2, 4) // black seed
+  ellipse(142, 33, 2, 4) // black seed
+  ellipse(140, 51, 2, 4) // black seed
+  ellipse(179, 28, 2, 4) // black seed
+  ellipse(150, 67, 2, 4) // black seed
 
 
 
