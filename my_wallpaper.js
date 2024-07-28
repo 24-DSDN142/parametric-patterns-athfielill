a41dae4c8f91c31328c1666ea98f7e27f31cd090
athfielill <lilly.athfield@gmail.com>
2024-07-28 12:03:26 +1200

    tidying code

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,6 @@
 //your parameter variables go here!
-let rect_width = 20;
-let rect_height = 20;
+// let rect_width = 20;
+// let rect_height = 20;
 
 
 function setup_wallpaper(pWallpaper) {
@@ -14,42 +14,82 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.grid_settings.row_offset = 0;
 }
 
+
+
+
+
 function wallpaper_background() {
   background(212, 245, 255); //light blue green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-
   angleMode(RADIANS)
+  var fruit = 1
+  if (fruit == 1) {
+    DrawLemonSlice(100, 20);
+  } else if (fruit == 2) {
+    DrawWatermelon();
+  } else if (fruit == 3) {
+    DrawFullLemon(10, 10);
+  }
 
 
-  // DrawLemonSlice(200 / 2, 200 / 2);
-
-
-
-  // let arc1x = 100
-  // let arc1y = 100
-
-  // strokeWeight(0);
-  // fill(255, 173, 20);//dark yellow
-  // arc(arc1x, arc1y, 100, 200, 20, PI);//outer semi circle
-
-  // stroke(255, 255, 255);
-  // strokeWeight(2);
-  // fill(255, 227, 89);//lighter yellow
-  // ellipse(100, 110, 90, 80);
-
-  // line(arc1x, arc1y, arc1x, arc1y + 45);
-
-  // line(arc1x, arc1y, arc1x - 31, arc1y + 32);
-  // line(arc1x, arc1y, arc1x + 31, arc1y + 31);
-  // line(arc1x - 45, arc1y, arc1x + 45, arc1y);
+}
 
 
 
+
+
+function DrawFullLemon(arc1x, arc1y) {
+
+
+  strokeWeight(0);
+  fill(255, 173, 20);//dark yellow
+  arc(arc1x, arc1y, 100, 200, 20, PI);//outer semi circle
+
+  stroke(255, 255, 255);
+  strokeWeight(2);
+  fill(255, 227, 89);//lighter yellow
+  ellipse(arc1x, arc1y, 90, 80);
+
+  line(arc1x, arc1y, arc1x, arc1y + 45);
+
+  line(arc1x, arc1y, arc1x - 31, arc1y + 32);
+  line(arc1x, arc1y, arc1x + 31, arc1y + 31);
+  line(arc1x - 45, arc1y, arc1x + 45, arc1y);
+
+}
+
+function DrawLemonSlice(arc1x, arc1y) {
+
+
+
+  strokeWeight(0);
+  fill(255, 173, 20);//dark yellow
+  arc(arc1x, arc1y, 100, 100, 0, PI);//outer semi circle
+
+  stroke(255, 255, 255);
+  strokeWeight(2);
+  fill(255, 227, 89);//lighter yellow
+  arc(arc1x, arc1y, 90, 90, 0, PI);//inner semi circle
+
+  line(arc1x, arc1y, arc1x, arc1y + 45);
+
+  line(arc1x, arc1y, arc1x - 31, arc1y + 32);
+  line(arc1x, arc1y, arc1x + 31, arc1y + 31);
+  line(arc1x - 45, arc1y, arc1x + 45, arc1y);
+
+}
+
+
+function DrawWatermelon() {
+
+  let seedX = 3
+  let seedY = 4
 
   //watermelon 1
-  stroke(235, 70, 89)
+
+  stroke(235, 70, 89)// stroke red
   fill(235, 70, 89)// red
   triangle(20, 75, 60, 20, 95, 75); // red triangle
 
@@ -70,13 +110,13 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
   stroke(5)
   fill(5); //black
-  ellipse(60, 35, 2, 4) // black seed
-  ellipse(47, 45, 2, 4) // black seed
-  ellipse(70, 50, 2, 4) // black seed
-  ellipse(52, 60, 2, 4) // black seed
-  ellipse(35, 67, 2, 4) // black seed
-  ellipse(79, 70, 2, 4) // black seed
-  ellipse(60, 75, 2, 4) // black seed
+  ellipse(60, 35, seedX, seedY) // black seed
+  ellipse(47, 45, seedX, seedY) // black seed
+  ellipse(70, 50, seedX, seedY) // black seed
+  ellipse(52, 60, seedX, seedY) // black seed
+  ellipse(35, 67, seedX, seedY) // black seed
+  ellipse(79, 70, seedX, seedY) // black seed
+  ellipse(60, 75, seedX, seedY) // black seed
 
   // watermelon 2
 
@@ -101,35 +141,14 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
   stroke(5)
   fill(5); //black
-  ellipse(160, 31, 2, 4) // black seed
-  ellipse(127, 28, 2, 4) // black seed
-  ellipse(160, 50, 2, 4) // black seed
-  ellipse(142, 33, 2, 4) // black seed
-  ellipse(140, 51, 2, 4) // black seed
-  ellipse(179, 28, 2, 4) // black seed
-  ellipse(150, 67, 2, 4) // black seed
+  ellipse(160, 31, seedX, seedY) // black seed
+  ellipse(127, 28, seedX, seedY) // black seed
+  ellipse(160, 50, seedX, seedY) // black seed
+  ellipse(142, 33, seedX, seedY) // black seed
+  ellipse(140, 51, seedX, seedY) // black seed
+  ellipse(179, 28, seedX, seedY) // black seed
+  ellipse(150, 67, seedX, seedY) // black seed
 
 
 
 }
-
-// function DrawLemonSlice(arc1x, arc1y) {
-
-
-
-//   strokeWeight(0);
-//   fill(255, 173, 20);//dark yellow
-//   arc(arc1x, arc1y, 100, 100, 0, PI);//outer semi circle
-
-//   stroke(255, 255, 255);
-//   strokeWeight(2);
-//   fill(255, 227, 89);//lighter yellow
-//   arc(arc1x, arc1y, 90, 90, 0, PI);//inner semi circle
-
-//   line(arc1x, arc1y, arc1x, arc1y + 45);
-
-//   line(arc1x, arc1y, arc1x - 31, arc1y + 32);
-//   line(arc1x, arc1y, arc1x + 31, arc1y + 31);
-//   line(arc1x - 45, arc1y, arc1x + 45, arc1y);
-
-// }
